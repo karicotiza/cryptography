d8183e17bc303c5fb08ea6bb03140487acd784e2
karicotiza <Kur7umerovD@Gmail.com>
2022-09-09 22:27:30 +0300

    Написал систему

---
+++ Лабораторная 1/main.py
@@ -0,0 +1,100 @@
+from random import sample
+
+
+class Encryptor:
+    def __init__(self) -> None:
+        self.BITS = 17
+        self.LETTER_SUBSTITUTION_SIZE = {
+            2: "хжшюцщэфёъvkxjqz",
+            3: "ыьгзбчйypb",
+            4: "рвлкмдпуяdlcumwfg",
+            5: "оеаинтсetaoinshr",
+        }
+
+        self.key: dict = self.__make_key()
+
+    def __make_key(self) -> dict:
+        encryption_key = dict()
+        for key, value in self.LETTER_SUBSTITUTION_SIZE.items():
+            numbers = sample(
+                range(
+                    10 ** (key - 1),
+                    10 ** key - 1
+                ),
+                len(value) * key
+            )
+
+            for index in range(len(value)):
+                memory = []
+                for _ in range(key):
+                    memory.append(numbers[0])
+                    numbers.pop(0)
+                encryption_key[value[index]] = memory
+
+        return encryption_key
+
+    def encrypt(self, text: str) -> str:
+        text = [letter for letter in text.lower() if letter.isalpha()]
+
+        ciphertext = ""
+        used_letters = dict()
+        for letter in text:
+            if (letter not in used_letters) or (used_letters[letter] == len(self.key[letter]) - 1):
+                used_letters[letter] = 0
+            else:
+                used_letters[letter] += 1
+            substitution = str(self.key[letter][used_letters[letter]])
+            substitution = "0" * (self.BITS - len(bin(int(substitution))) + 2) + str(bin(int(substitution)))[2:]
+            ciphertext += substitution
+
+        return ciphertext
+
+    def save_key(self, path: str) -> None:
+        with open(path, "w", encoding="utf-8") as file:
+            for key, value in self.key.items():
+                file.write(
+                    f"{key}: {value}\n"
+                )
+
+
+class Decryptor:
+    def __init__(self, path_to_key: str) -> None:
+        self.BITS = 17
+        self.key: dict = self.__load_key(path_to_key)
+
+    @staticmethod
+    def __load_key(path_to_key: str) -> dict:
+        decryption_key = dict()
+        with open(path_to_key, "r", encoding="utf-8") as file:
+            for line in file:
+                key, value = line[:-1].split(": ")
+                decryption_key[key] = eval(value)
+
+        return decryption_key
+
+    def decrypt(self, text: str, ) -> str:
+        text = [text[index - self.BITS: index] for index in range(self.BITS, len(text) + 1, self.BITS)]
+        text = [int(section, 2) for section in text]
+
+        decrypted_text = ""
+        for section in text:
+            for key, value in self.key.items():
+                if section in value:
+                    decrypted_text += key
+
+        return decrypted_text
+
+
+if __name__ == "__main__":
+    while True:
+        encryptor = Encryptor()
+        user_input = str(input("Текст для шифрования: "))
+        ciphertext = encryptor.encrypt(user_input)
+
+        encryptor.save_key("key.txt")
+
+        decryptor = Decryptor("key.txt")
+        decrypted_text = decryptor.decrypt(ciphertext)
+
+        print("Текст в зашифрованном виде: ", ciphertext, "\n")
+        print("Расшифрованный шифротекст: ", decrypted_text, "\n")
